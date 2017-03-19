3a1184b9cf0f834e853573e96d597a31b373e1ef
vascudo <vascudo.crisis@gmail.com>
2017-03-19 22:24:15 +0300

    Enable CORS

--- BookShelf/Configuration/WebApiConfig.cs
+++ BookShelf/Configuration/WebApiConfig.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace BookShelf.Configuration
 {
@@ -7,6 +8,9 @@ namespace BookShelf.Configuration
         internal static HttpConfiguration RegisterWebApi(this HttpConfiguration config)
         {
             config.MapHttpAttributeRoutes();
+            
+            var corsAttr = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(corsAttr);
 
             return config;
         }
